7db36060b16abffe66d076ee756dc2f09c09aa36
Max Valencia <maxvalencia156@gmail.com>
2023-10-05 16:06:13 -0600

    [feuture] add multiple-class and soft max

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 
 from models import NeuronalRed
 from enums import Calc
+import csv
+import numpy as np
 
 def main():
-    red = NeuronalRed(layers=3,neurons=[3,4,1],epochs=4000,x=[[0,0,1],[0,1,1],[1,0,1],[1,1,1]],d=[[0],[1],[1],[0]],step=0.4,calc=Calc.CROSS_ENTROPY)
+
+    with open('iris.csv', 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    data_array = np.array(data)
+    X = np.array(data_array[1:,0:-1],dtype=float)
+    d = np.array(data_array[1:,-1:])
+    norms = np.linalg.norm(X, axis=1)
+    for x in range(0,len(X)):
+        for y in range(0,len(X[x])):
+            X[x][y] = X[x][y] / norms[x]
+    print(X)
+    D = []
+    hotEncoding = {
+        'Setosa' : [1,0,0],
+        'Versicolor' : [0,1,0],
+        'Virginica' : [0,0,1]
+    }
+    for x in d :
+        D.append(hotEncoding[x[0]])
+
+    red = NeuronalRed(layers=3,neurons=[4,8,3],epochs=700,x=X,d=D,step=0.4,calc=Calc.CROSS_ENTROPY,clasification_multiple=True)
     red.start_training()
     red.test()
 
-    # red2 = NeuronalRed(layers=3,neurons=[2,4,1],epochs=4500,x=[[0,0],[0,1],[1,0],[1,1]],d=[[0],[1],[1],[0]],step=0.9,calc=Calc.CROSS_ENTROPY)
+    # red2 = NeuronalRed(layers=3,neurons=[2,4,1],epochs=10000,x=[[0,0],[0,1],[1,0],[1,1]],d=[[0],[1],[1],[0]],step=0.7,calc=Calc.CROSS_ENTROPY)
     # red2.start_training()
     # red2.test()
 
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import numpy as np
 from enums import Calc
 
 class NeuronalRed:
-    def __init__(self,layers,neurons,x,d,epochs,step=0.2,calc=Calc.GENERALLY_DELTA):
+    def __init__(self,layers,neurons,x,d,epochs,step=0.2,calc=Calc.GENERALLY_DELTA,clasification_multiple=False):
         # Layers of neural red
         self.layers   = []
         # Output of neural red
@@ -21,7 +21,10 @@ class NeuronalRed:
         self.index_training_data = 0
         # error convergence
         self.J = []
+        # Type of calculate
         self.calc = calc
+        # if our neural red does clasification multiple
+        self.clasification_multiple = clasification_multiple
         for i in range(0,layers):
             input_layer = False
             output_layer  = False
@@ -46,7 +49,10 @@ class NeuronalRed:
                         layer.calculate_layer()
                     else:
                         layer.output = self.X[i]
-                error = error + pow((self.D[i][0] - self.layers[-1].output[0]),2)
+                if (self.clasification_multiple == False):
+                    error = error + pow((self.D[i][0] - self.layers[-1].output[0]),2)
+                else:
+                    error = 0
                 for i in range(len(self.layers) - 1,0,-1):
                     if not self.layers[i].input_layer:
                         self.layers[i].backpropagation()
@@ -57,9 +63,6 @@ class NeuronalRed:
         plt.ylabel('error')
         plt.title('average square error')
         plt.show()
-        for layer in self.layers:
-            for neuron in layer.neurons:
-                print(neuron.W)
     
     def test(self):
         for i in range(0,len(self.D)):
@@ -88,10 +91,14 @@ class Layer:
         self.output = [0] * neurons[self.index]
         # Deltas of layer
         self.deltas = [0] * neurons[self.index]
+        # Output of layer
+        self.output_b = [0] * neurons[self.index]
         if output_layer:
             self.next_length_w = 0
         else:
             self.next_length_w = neurons[index + 1]
+        # factor to use with softmax
+        self.factor_soft_max = 0
         for i in range(0,neurons[index]):
             _neuron = Neuron(i,self.next_length_w,self)
             self.neurons.append(_neuron)
@@ -99,6 +106,17 @@ class Layer:
     def calculate_layer(self):
         for neuron in self.neurons:
             neuron.calculate_neuron()
+        if(self._neural_red.clasification_multiple == True and self.output_layer == True):
+            # print("last layer ")
+            # print(self.output_layer)
+            # print("output b")
+            # print(self.output_b)
+            self.factor_soft_max = np.sum(self.output_b)
+            # print(self.factor_soft_max)
+            for neuron in self.neurons:
+                neuron.calculate_neuron_with_softmax()
+            self.factor_soft_max = 0
+            # print(self.output)
     
     def get_weights_by_index(self,index):
         _w = []
@@ -140,6 +158,9 @@ class Neuron:
     def calculate_error(self):
         if self._layer.output_layer:
             red = self._layer._neural_red
+            # print("errors")
+            # print(red.D[red.index_training_data][self.index])
+            # print(self.output)
             self.error = red.D[red.index_training_data][self.index] - self.output
         else:
             _w  = np.array(self.W)
@@ -164,7 +185,19 @@ class Neuron:
         xi = np.transpose(np.array(_prev_layer.output))
         w  = np.array(_prev_layer.get_weights_by_index(self.index))
         v = (w @ xi)
-        self.output = self.activation(v)
+        if (self._layer._neural_red.clasification_multiple == False):
+            self.output = self.activation(v)
+        else:
+            if (self._layer.output_layer == False):
+                self.output = self.activation(v)
+            else:
+                aux = np.exp(v)
+                self.output = aux
+                # print(f"Index {self.index} v {v} e^v {aux}")
+                self._layer.output_b[self.index] = aux
+                # aux = np.exp(v)
+                # aux2 = self._layer.factor_soft_max
+                # self._layer.factor_soft_max = aux2 + aux
         self._layer.output[self.index] = self.output
     
     def get_error(self):
@@ -176,6 +209,15 @@ class Neuron:
     
     def get_next_layer(self):
         return self._layer._neural_red.layers[self._layer.index + 1]
+    
+    def calculate_neuron_with_softmax(self):
+        # print(f"output before {self.output}")
+        self.output = self.soft_max()
+        self._layer.output[self.index] = self.output
+    
+    def soft_max(self):
+        # print(f"using {self._layer.factor_soft_max}")
+        return (self.output / self._layer.factor_soft_max)
     
     def __str__(self):
         return f"Im a Neuron and I have {len(self.W)} weights"
